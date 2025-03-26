c64bf9ab49e9e5f1335407c09e7c0cdca2086ea7
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-03-25 23:00:10 -0300

    update webhook endpoint

--- routers/webhook.py
+++ routers/webhook.py
@@ -1,10 +1,30 @@
 from fastapi import APIRouter
 from models.webhooks import PullRequestWebhook
+from services.auth import gen_jwt
 
 webhook_router = APIRouter()
 
 @webhook_router.post("/github/webhook")
 async def github_webhook(payload: PullRequestWebhook):
-    print('payload: ', payload)
-    
+    # print('payload: ', payload)
+    printed = False
+
+    if (payload.action == 'created' and payload.installation): 
+        # print('payload.installation: ', payload.installation)
+        #gen_jwt()
+        printed = True
+
+    if (payload.workflow_job):
+        # print('payload.workflow_job: ', payload.workflow_job)
+        printed = True
+
+    if (payload.workflow_run):
+        # print('payload.workflow_run: ', payload.workflow_run)
+        print('payload: ', payload)
+
+        printed = True
+
+    if not printed:
+        print('payload: ', payload)
+        
     return None
