f70f0dcbdc42e71412823c30e5ad948bca7bae97
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-03-25 22:59:12 -0300

    Add new ProductUserLogin model

--- models/users.py
+++ models/users.py
@@ -6,3 +6,7 @@ class User(BaseModel):
     name: str = Field(..., description="User/Organization name")
     user_name: str = Field(..., description="UserName")
     token: str = Field(..., description="Access token")
+
+class ProductUserLogin(BaseModel):
+    username: str = Field(..., description="User/Organization name")
+    password: str = Field(..., description="Password")
