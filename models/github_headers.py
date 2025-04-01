f31dac82a483000b0b4791427794282dbc1719fa
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-04-01 09:35:18 -0300

    update Github headers model

--- models/github_headers.py
+++ models/github_headers.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 class GitHubHeaders(BaseModel):
     Authorization: str = Field(..., description="Auth Bearer")
+    Accept: Optional[str] = Field(default=None, description="Accept Encoding")
